05d5c294af453296499bd4fac1d6459d5744e746
Erica <ericaesnider@gmail.com>
2018-01-19 16:20:13 -0600

    add async fetch

---
+++ async/fetch/app.js
@@ -0,0 +1,65 @@
+
+// fetch API: promised-based async requests
+
+const key = "c3f46be33b42052c824c21899cea4ab783ffb2618961597c5a9624592c24ac96";
+const form = document.querySelector('#search-form');
+const searchField = document.querySelector('#search-keyword')
+const responseContainer = document.querySelector('#response-container');
+let searchedForText;
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  searchedForText = searchField.value;
+  // searchedForText='hippos';
+  responseContainer.innerHTML = '';
+  let url = `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`;
+  fetch(url, {
+    headers: {
+      Authorization: `Client-ID ${key}`
+    }
+  })
+  .then(responseError)
+  .then(response => {
+    let jsonResponse = response.json();
+    // console.log(jsonResponse);
+    return jsonResponse;
+  })
+  .then(addImage)
+  .catch(e => requestError(e));
+
+  // NOTE .catch runs when network error is encountered, although this usually means permissions issues or similar (e.g. bad origin)
+});
+
+function responseError(response) {
+  if (!response.ok) {
+    console.log("There was an error with the response.")
+    console.log(response.statusText)
+  }
+  return response;
+}
+
+function addImage(data) {
+  clearSearch();
+  let htmlContent = '';
+  if(data && data.results && data.results[0]) {
+    const firstImage = data.results[0];
+    htmlContent = `
+      <figure>
+        <img src="${firstImage.urls.small}" alt="${searchedForText}" />
+        <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+      </figure>
+    `;
+  } else {
+    htmlContent = '<div>No images available</div>';
+  }
+  responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+}
+
+function requestError(err) {
+  console.log("There was an error with the request.")
+  console.log(err)
+}
+
+function clearSearch() {
+  searchField.value = '';
+}
