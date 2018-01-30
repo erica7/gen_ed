11eac3c7372edb584017fd0246699fc3e6419e7d
Erica <ericaesnider@gmail.com>
2018-01-30 16:20:13 -0600

    get new API key; deactivate committed API key; add config file to html; replace key with config variable in js

--- async/fetch/app.js
+++ async/fetch/app.js
@@ -1,7 +1,7 @@
 
 // fetch API: promised-based async requests
 
-const key = "c3f46be33b42052c824c21899cea4ab783ffb2618961597c5a9624592c24ac96";
+const key = UNSPLASH_KEY;
 const form = document.querySelector('#search-form');
 const searchField = document.querySelector('#search-keyword')
 const responseContainer = document.querySelector('#response-container');
